cfa3d81a07b6d07675a0d45b3d984b660d017657
Shady47324 <Shady47324>
2025-07-24 19:11:59 +0100

    amélioration backend

--- llama-backend/src/main/java/com/llama/llamabackend/dto/LlamaResponseDTO.java
+++ llama-backend/src/main/java/com/llama/llamabackend/dto/LlamaResponseDTO.java
@@ -1,85 +1,26 @@
 package com.llama.llamabackend.dto;
 
+import lombok.Getter;
 import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 import java.util.List;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class LlamaResponseDTO {
-
     private String output;
     private String language;
     private List<String> errors;
     private List<String> corrections;
-
     private int inputTokens;
     private int outputTokens;
     private double responseTimeMs;
-    @Setter
     private String originalCode;
-    @Setter
     private String correctedCode;
-    @Setter
     private List<Integer> originalHighlightLines;
-    @Setter
     private List<Integer> correctedHighlightLines;
-
-
-    // Getters et setters pour tout
-
-    public String getOutput() {
-        return output;
-    }
-
-    public void setOutput(String output) {
-        this.output = output;
-    }
-
-    public String getLanguage() {
-        return language;
-    }
-
-    public void setLanguage(String language) {
-        this.language = language;
-    }
-
-    public List<String> getErrors() {
-        return errors;
-    }
-
-    public void setErrors(List<String> errors) {
-        this.errors = errors;
-    }
-
-    public List<String> getCorrections() {
-        return corrections;
-    }
-
-    public void setCorrections(List<String> corrections) {
-        this.corrections = corrections;
-    }
-
-    public int getInputTokens() {
-        return inputTokens;
-    }
-
-    public void setInputTokens(int inputTokens) {
-        this.inputTokens = inputTokens;
-    }
-
-    public int getOutputTokens() {
-        return outputTokens;
-    }
-
-    public void setOutputTokens(int outputTokens) {
-        this.outputTokens = outputTokens;
-    }
-
-    public double getResponseTimeMs() {
-        return responseTimeMs;
-    }
-
-    public void setResponseTimeMs(double responseTimeMs) {
-        this.responseTimeMs = responseTimeMs;
-    }
-
 }
